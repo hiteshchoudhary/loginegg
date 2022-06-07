8339e505b8022ea00e8c3bea8438778022312e69
Hitesh Choudhary <hiteshchoudhary@Hiteshs-MacBook-Pro.local>
2022-06-07 11:25:50 +0530

    first git push for frontegg

--- src/App.js
+++ src/App.js
@@ -1,23 +1,62 @@
 import logo from './logo.svg';
 import './App.css';
+import {useEffect} from 'react'
+
+import { ContextHolder } from '@frontegg/rest-api';
+import { AdminPortal, useAuth, useLoginWithRedirect } from "@frontegg/react";
 
 function App() {
+
+  const { user, isAuthenticated } = useAuth();
+  const loginWithRedirect = useLoginWithRedirect();
+
+  // Uncomment this to redirect to login automatically
+  useEffect(() => {
+    if (!isAuthenticated) {
+  loginWithRedirect();
+    }
+  }, [isAuthenticated, loginWithRedirect]);
+
+  const logout = () => {
+    const baseUrl = ContextHolder.getContext().baseUrl;
+    window.location.href = `${baseUrl}/oauth/logout?post_logout_redirect_uri=${window.location}`;
+  };
+
+  // admin user
+  const handleClick = () => {
+    AdminPortal.show()
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {isAuthenticated ? (
+        <div className='card'>
+          <div>
+            <img src={user?.profilePictureUrl} alt={user?.name} />
+          </div>
+          <div className="detail">
+            <span className="heading">Name: {user?.name}</span>
+            <span className="heading">Name: {user?.email}</span>
+          </div>
+          <div>
+            <button
+            onClick={() => logout()}
+            className="button">Click to logout</button>
+            <button
+            onClick={() => handleClick()}
+            className="button">View full Profile</button>
+          </div>
+        </div>
+      ): 
+      (
+        <div>
+        <button 
+        onClick={() => loginWithRedirect()}
+        className="button">
+        Click me to login
+        </button>
+        </div>
+      ) }
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -4,11 +4,18 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import { FronteggProvider } from '@frontegg/react';
+
+const contextOptions = {
+  baseUrl: 'https://app-mkcoh191862s.frontegg.com',
+  clientId: '34c09c95-7a07-4af0-9da1-76a90420d1a5'
+};
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <FronteggProvider contextOptions={contextOptions} hostedLoginBox={true}>>
     <App />
-  </React.StrictMode>
+  </FronteggProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
